0a68ca33d504ea336d19accc29df5b8f836ec333
Sim Dim <simdim555@gmail.com>
2018-04-15 14:14:43 +0300

    change index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
-import draggableContainer from './draggableContainer';
-import { draggableHandle, draggable } from './draggable';
-import droppable from './droppable';
-
-export default {
-    draggableContainer,
-    draggable,
-    droppable,
-    draggableHandle
-};
+export * from './draggableContainer';
+export * from './draggable';
+export * from './droppable';
