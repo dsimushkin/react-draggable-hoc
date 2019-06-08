1155ceadbfbf93a8e8917dea306af0996969cf2d
Sim Dim <simdim555@gmail.com>
2019-06-08 16:09:33 +0300

    Updates

---
+++ precache-manifest.5823bbb20ecbaf49554c7509219d9fb3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5221b96029367c3bc687b1fa4dd8d20f",
+    "url": "/react-draggable-hoc/index.html"
+  },
+  {
+    "revision": "be96daba2326d4c39669",
+    "url": "/react-draggable-hoc/static/css/main.1e86d113.chunk.css"
+  },
+  {
+    "revision": "538f58e56bfe0035538a",
+    "url": "/react-draggable-hoc/static/js/2.3246a3db.chunk.js"
+  },
+  {
+    "revision": "be96daba2326d4c39669",
+    "url": "/react-draggable-hoc/static/js/main.d5d084d4.chunk.js"
+  },
+  {
+    "revision": "f424124805d48608e57e",
+    "url": "/react-draggable-hoc/static/js/runtime~main.1e573ae2.js"
+  }
+]);
