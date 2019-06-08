99e9f3173b2172b80eabe0f9ba97435557873db9
Sim Dim <simdim555@gmail.com>
2019-06-08 16:21:36 +0300

    upgrade rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,34 @@
-import typescript from 'rollup-plugin-typescript2'
-import pkg from './package.json'
+import typescript from "rollup-plugin-typescript2";
+import commonjs from "rollup-plugin-commonjs";
+import external from "rollup-plugin-peer-deps-external";
+import resolve from "rollup-plugin-node-resolve";
+
+import pkg from "./package.json";
+
 export default {
     input: 'src/index.ts',
     output: [
-        {
-            file: pkg.main,
-            format: 'cjs',
-        }
-    ],
-    external: [
-        ...Object.keys(pkg.peerDependencies || {}),
-        ...Object.keys(pkg.dependencies || {}),
+      {
+        file: pkg.main,
+        format: "cjs",
+        exports: "named",
+        sourcemap: true
+      },
+      {
+        file: pkg.module,
+        format: "es",
+        exports: "named",
+        sourcemap: true
+      }
     ],
     plugins: [
-        typescript({
-            clean: true
-        }),
-    ],
+      external(),
+      resolve(),
+      typescript({
+        rollupCommonJSResolveHack: true,
+        exclude: "**/__tests__/**",
+        clean: true
+      }),
+      commonjs()
+    ]
 }
