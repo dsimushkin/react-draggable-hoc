6645a3f930d8e2a055f1fc249f521364ad52b66f
Dmitriy Sim <simdim555@gmail.com>
2020-03-21 22:01:01 +0300

    commit added

---
+++ src/useDragStopPropagation.ts
@@ -0,0 +1,14 @@
+import useDragPhaseListener from "./useDragPhaseListener";
+import { DragPhase } from "./helpers";
+
+function useDragStopPropagation(
+  ref: React.RefObject<any>,
+  ...phases: DragPhase[]
+) {
+  if (ref == null) {
+    console.warn("Do not use useDragStopPropagation without a ref");
+  }
+  useDragPhaseListener(e => e.stopPropagation(), phases, ref);
+}
+
+export default useDragStopPropagation;
