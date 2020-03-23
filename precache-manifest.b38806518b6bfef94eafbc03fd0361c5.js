c24ca04f34aa05b18b446a0c28aa859f38e067d6
Dmitriy Sim <simdim555@gmail.com>
2020-03-23 13:15:09 +0300

    Updates

---
+++ precache-manifest.b38806518b6bfef94eafbc03fd0361c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfda3f5259f456aeac7295678c263fc4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e07eeee24aeccae54cee",
+    "url": "/static/css/main.4fcaf3dd.chunk.css"
+  },
+  {
+    "revision": "c1f3ed100fe90a10eb95",
+    "url": "/static/js/2.d9712641.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.d9712641.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e07eeee24aeccae54cee",
+    "url": "/static/js/main.c5f86c6a.chunk.js"
+  },
+  {
+    "revision": "90adf2f23a14bedb2261",
+    "url": "/static/js/runtime-main.21c171de.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-draggable-hoc/precache-manifest.e030461b690207795849d6f80cbbb5c8.js"
+  "/precache-manifest.b38806518b6bfef94eafbc03fd0361c5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-draggable-hoc/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
