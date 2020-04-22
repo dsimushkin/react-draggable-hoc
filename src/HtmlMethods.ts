bef95da08c2fdfdb22fafd815b6a317651f5358b
Dmitriy Sim <simdim555@gmail.com>
2020-04-22 16:25:03 +0300

    defaultDroppableMethod overloading

--- src/HtmlMethods.ts
+++ src/HtmlMethods.ts
@@ -1,14 +1,25 @@
 import HtmlDndObserver from "./HtmlDndObserver";
 
-export function defaultDroppableMethod<T>(
+export function defaultDroppableMethod<T, N extends Element>(
   state: HtmlDndObserver<T>["state"],
-  ref: React.RefObject<any>,
+  n: N | React.RefObject<N>,
 ) {
-  const node = ref.current;
-  if (state.current && node) {
-    const { x, y } = state.current;
-    return document.elementsFromPoint(x, y).some((n) => node.contains(n));
+  if (n == null) return false;
+  const node = n instanceof Element ? n : n.current;
+  if (node != null) {
+    return getElementsFromPointInState(state).some((n) => node.contains(n));
   }
 
   return false;
+}
+
+export function getElementsFromPointInState<T>(
+  state: HtmlDndObserver<T>["state"],
+) {
+  if (state != null && state.current != null) {
+    const { x, y } = state.current;
+    return [].slice.call(document.elementsFromPoint(x, y));
+  }
+
+  return [];
 }
